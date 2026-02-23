468e20076f4002da02648b93cd3e430b6266c2e2
xl3619-maker <xl3619@columbia.edu>
2026-02-23 18:30:21 -0500

    BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,1 +1,17 @@
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    if ppy == 0:
+        return face / (1 + y) ** m
 
+    n = int(m * ppy)
+    r = y / ppy
+    coupon = face * couponRate / ppy
+
+    bondPrice = 0
+
+    for t in range(1, n + 1):
+        bondPrice = bondPrice + coupon / ((1 + r) ** t)
+
+    bondPrice = bondPrice + face / ((1 + r) ** n)
+
+    
+    return bondPrice
