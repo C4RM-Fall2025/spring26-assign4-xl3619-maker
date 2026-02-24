5d62676b8e6b19ecb42f746485892edce6ce7180
xl3619-maker <xl3619@columbia.edu>
2026-02-23 19:20:29 -0500

    BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,17 +1,14 @@
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 0:
-        return face / (1 + y) ** m
 
-    n = int(m * ppy)
-    r = y / ppy
+    n = int(m * ppy)              # total number of periods
+    r = y / ppy                   # periodic yield
     coupon = face * couponRate / ppy
 
     bondPrice = 0
 
     for t in range(1, n + 1):
-        bondPrice = bondPrice + coupon / ((1 + r) ** t)
+        bondPrice += coupon / ((1 + r) ** t)
 
-    bondPrice = bondPrice + face / ((1 + r) ** n)
+    bondPrice += face / ((1 + r) ** n)
 
-    
     return bondPrice
